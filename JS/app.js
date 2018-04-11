f7b3ba737365a11f32152ce9d8759720940899f3
Lainix <linkelin8@gmail.com>
2018-04-11 13:05:43 -0500

    Cambios en los comentarios y la condicional

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,9 @@
 var creditNumber = prompt("Ingresa el numero de tarjeta");// obtener el numero de tarjeta mediante un prompt
 
 function isValidCard(creditNumber){ // creamos la funcion de isValidCard
-    if(creditNumber.indexOf(" ") !== -1){ //\s
-        creditNumber = prompt("Ingresa el numero de tarjeta sin spacios"); // si no cumple el while se estara repitiendo hasta que se cumpla
+    var noValido = /\s/; // variable para caso de que tenga campo vacío
+    if(noValido.test(creditNumber)){ //para validar que no tenga campo vacío
+        creditNumber = prompt("Ingresa el numero de tarjeta sin spacios"); // si no se cumple preguntara de nuevo
     } 
     /*else if(creditNumber === null){
         //creditNumber = prompt("Ingresa el numero de tarjeta")
@@ -28,12 +29,12 @@ function isValidCard(creditNumber){ // creamos la funcion de isValidCard
                     sum+= multiplication % 10; // se extrae el residuo de %10
                     multiplication = Math.floor(multiplication/10) // redondeamos el cociente de la division con Math.floor
                 }// while inpares
-                pairNumbers.push(sum) // empujamos los numeros en las posiciones par
+                pairNumbers.push(sum) // numeros de las posiciones par
             }else {
-                pairNumbers.push(multiplication)// empujamos los numeros de las posiciones inpar
+                pairNumbers.push(multiplication)// numeros de las posiciones inpar
             } //else pairNumbers.push(multiplication)
         }else
-        pairNumbers.push(parseInt(creditNumberReverse[i]));// comvertimos el string en entero y colocamos en el array
+        pairNumbers.push(parseInt(creditNumberReverse[i]));// convertimos el string en entero y lo colocamos en el array
     }
     var numberValidate = 0; // creamos una variable numberValidate para dar valor de inicio 0
     for(var j=0; j<pairNumbers.length; j++){// se itineran los numeros pares
